cdab6ae4446cd5b2b41ec87f2ec4ccd68848e172
qarce76 <qarce76@30fab446-694b-0410-acbb-ad9843a15c28>
2011-10-06 05:27:22 +0000

    code compiles... not tested

--- branches/qarce76/avr-uip-netconf/projects/TuxGraphics-volt-amp.m328p/main.c
+++ branches/qarce76/avr-uip-netconf/projects/TuxGraphics-volt-amp.m328p/main.c
@@ -11,13 +11,15 @@
 #include "enc28j60.h"
 #include "apps-conf.h"
 #include <string.h>
+
+#include "net_conf.h"
+
 #define BUF ((struct uip_eth_hdr *)&uip_buf[0])
 
 
 
 //EEPROM parameters (TCP/IP parameters)
 
-struct timer dhcp_timer;
 
 int main(void)
 {
@@ -44,6 +46,9 @@ int main(void)
     // must be done or sometimes arp doesn't work
     uip_arp_init();
 	
+	// load the network configs from eeprom
+	net_conf_init();
+
     // start up the webserver
     //httpd_init();
 	//simple_httpd_init();
@@ -95,12 +100,7 @@ int main(void)
 				timer_reset(&arp_timer);
 				uip_arp_timer();
 			}
-		} else if (_enable_dhcp && timer_expired(&dhcp_timer)) {
-            // for now turn off the led when we start the dhcp process
-            //led_low();
-            dhcpc_renew();
-            timer_reset(&dhcp_timer);
-        }
+		}
 /// 		while(network_sending()) {asm("nop");}; //wait untill packet is sent away
 	}
 	return 0;
@@ -117,54 +117,3 @@ void uip_log(char *m)
 /*---------------------------------------------------------------------------*/
 
 
-void dhcpc_configured(const struct dhcpc_state *s)
-{
-    uip_ipaddr_t addr;
-
-    // byte swap the network info
-    _ip_addr[0] = (s->ipaddr[0]);
-    _ip_addr[1] = (s->ipaddr[0]) >> 8;
-    _ip_addr[2] = (s->ipaddr[1]);
-    _ip_addr[3] = (s->ipaddr[1]) >> 8;
-
-    _net_mask[0] = (s->netmask[0]);
-    _net_mask[1] = (s->netmask[0]) >> 8;
-    _net_mask[2] = (s->netmask[1]);
-    _net_mask[3] = (s->netmask[1]) >> 8;
-
-    _gateway[0] = (s->default_router[0]);
-    _gateway[1] = (s->default_router[0]) >> 8;
-    _gateway[2] = (s->default_router[1]);
-    _gateway[3] = (s->default_router[1]) >> 8;
-
-#ifdef DHCP_DEBUG
-    eeprom_write_block (_ip_addr, &ee_ip_addr, 4);
-    eeprom_write_block (_net_mask,&ee_net_mask,4);
-    eeprom_write_block (_gateway, &ee_gateway, 4);
-#endif
-    // re-init just in case
-	uip_setethaddr(my_eth_addr);
-
-    // set ip
-    uip_ipaddr(&addr, _ip_addr[0], _ip_addr[1], _ip_addr[2], _ip_addr[3]);
-    uip_sethostaddr(&addr);
-    
-    // set netmask
-    uip_ipaddr(&addr,_net_mask[0], _net_mask[1], _net_mask[2], _net_mask[3]);
-    uip_setnetmask(&addr);
-
-    // set gateway
-    uip_ipaddr(&addr,_gateway[0], _gateway[1], _gateway[2], _gateway[3]);
-    uip_setdraddr(&addr);
-
-//  code to use dhcp server lease time removed due to uint16_t overflow
-//  issues with calculating the time.  Just use 5 minutes instead.  
-    timer_set(&dhcp_timer, 5 * 60 * CLOCK_SECOND);
-
-#ifdef DHCP_DEBUG
-    // for now turn on the led when we get an ip
-    led_high();
-#endif
-}
-
-/*---------------------------------------------------------------------------*/
