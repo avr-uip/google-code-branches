4d042af07fe0910631efd1e8bc123e19a88dd84b
turbomustanggt@gmail.com <turbomustanggt@gmail.com@30fab446-694b-0410-acbb-ad9843a15c28>
2012-01-22 15:48:54 +0000

    Created a branch for dev

---
+++ halfmoon_tech/projects/Halfmoon_Tech.1284/clock-arch.h
@@ -0,0 +1,14 @@
+#ifndef __CLOCK_ARCH_H__
+#define __CLOCK_ARCH_H__
+
+//#include "global-conf.h"
+#include <stdint.h>
+
+typedef uint16_t clock_time_t;
+#define CLOCK_CONF_SECOND  20
+#include "clock.h"
+
+void clock_init(void);
+clock_time_t clock_time(void);
+
+#endif /* __CLOCK_ARCH_H__ */
---
+++ halfmoon_tech/projects/TuxGraphics-volt-amp.m328p/main.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include <avr/wdt.h>
+
+#include "global-conf.h"
+#include "timer.h"
+#include <util/delay.h>
+
+
+
+
+#include "uip_arp.h"
+#include "network.h"
+#include "enc28j60.h"
+#include "apps-conf.h"
+#include <string.h>
+
+#include "net_conf.h"
+#include "adc.h"
+
+
+#define BUF ((struct uip_eth_hdr *)&uip_buf[0])
+
+volatile uint16_t volt, cur;
+
+
+int main(void)
+{
+    /* Disable watchdog if enabled by bootloader/fuses */
+    MCUSR &= ~(1 << WDRF);
+    wdt_disable();
+
+	network_init();
+
+
+	CLKPR = (1<<CLKPCE);	//Change prescaler
+	CLKPR = 0;	//Use prescaler 2
+	//CLKPR = (1<<CLKPS0);	//Use prescaler 2
+	_delay_loop_1(0);
+	enc28j60Write(ECOCON, 2 & 0x7);	//Get a 12.5MHz signal from enc28j60
+	_delay_loop_1(0);
+
+	int i;
+	struct timer periodic_timer, arp_timer;
+
+	clock_init();
+
+	timer_set(&periodic_timer, CLOCK_SECOND);
+	timer_set(&arp_timer, CLOCK_SECOND * 10);
+
+	uip_init();
+    // must be done or sometimes arp doesn't work
+    uip_arp_init();
+	
+	// load the network configs from eeprom
+	net_conf_init();
+
+    // start telnet server
+	telnetd_init();
+
+	// set up the udp data stream
+	// configure the target system ip and port
+	uip_ipaddr_t ipaddr;
+	uip_ipaddr(&ipaddr, 192,168,2,99);
+	udpds_conf(ipaddr, 33, 8);
+
+
+	adc_init();
+
+
+	while(1){
+
+		volt = read_adc(0);
+		volt = ((((float)volt) / 1024) * 3300 * 2);
+		cur = (volt / 10);
+
+		uip_len = network_read();
+
+		if(uip_len > 0) {
+			if(BUF->type == htons(UIP_ETHTYPE_IP)){
+				uip_arp_ipin(); // arp seems to have issues w/o this
+				uip_input();
+				if(uip_len > 0) {
+					uip_arp_out();
+					network_send();
+				}
+			}else if(BUF->type == htons(UIP_ETHTYPE_ARP)){
+				uip_arp_arpin(); // this is correct
+				if(uip_len > 0){
+					network_send();
+				}
+			}
+
+		}else if(timer_expired(&periodic_timer)) {
+			timer_reset(&periodic_timer);
+
+
+			for(i = 0; i < UIP_CONNS; i++) {
+				uip_periodic(i);
+				if(uip_len > 0) {
+					uip_arp_out();
+					network_send();
+				}
+			}
+
+			#if UIP_UDP
+			for(i = 0; i < UIP_UDP_CONNS; i++) {
+				uip_udp_periodic(i);
+				if(uip_len > 0) {
+					uip_arp_out();
+					network_send();
+				}
+			}
+			#endif /* UIP_UDP */
+
+			if(timer_expired(&arp_timer)) {
+				timer_reset(&arp_timer);
+				uip_arp_timer();
+			}
+		}
+	}
+	return 0;
+}
+
+#if UIP_CONF_LOGGING == 1
+void uip_log(char *m)
+{
+//	sendString(m);
+	printf("%s", m);
+	//TODO: Get debug information out here somehow, does anybody know a smart way to do that?
+}
+#endif
+
+/*---------------------------------------------------------------------------*/
+
+int udpds_set_payload(void)
+{
+  return (1 + sprintf((char *)uip_appdata,"volt: %d cur: %d\n", volt, cur));
+}
+
+
